5da0a161c7432732aefc4476fe567f64fd1af394
renatomarf76 <renatomarf76@egmail.com>
2025-07-16 17:37:37 -0300

    12-agents-data-table

--- src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -62,7 +62,7 @@ export const DashboardSidebar = () => {
                     asChild
                     className={cn(
                       "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
-                      pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68/10"
+                      pathname === item.href && "bg-linear-to-r/oklch border-[#5D6B68]/10"
                     )}
                     isActive={pathname === item.href}
                   >
