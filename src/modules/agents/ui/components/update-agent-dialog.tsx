40ef5773ec9ce2c001f70b1a277c2fa823cc360a
renatomarf76 <renatomarf76@egmail.com>
2025-07-17 16:19:10 -0300

    15-update-delete-agent

---
+++ src/modules/agents/ui/components/update-agent-dialog.tsx
@@ -0,0 +1,30 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { AgentForm } from "./agent-form";
+import { AgentGetOne } from "../../types";
+
+interface Props {
+  open: boolean;
+  onOpenChange: (ope: boolean) => void;
+  initialValues: AgentGetOne;
+};
+
+export const UpdateAgentDialog = ({ 
+  open, 
+  onOpenChange, 
+  initialValues,
+}: Props) => {
+  return ( 
+    <ResponsiveDialog
+      title="Edit Agent"
+      description="Edit the agent details"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <AgentForm 
+        onSuccess={() => onOpenChange(false)}
+        onCancel={() => onOpenChange(false)}
+        initialValues={initialValues}
+      />
+    </ResponsiveDialog>
+   );
+};
